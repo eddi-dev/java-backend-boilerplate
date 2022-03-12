3a4fb66ce4de71c48739cd513d6d13df040bbc2f
hyeyoom <neoul_chw@icloud.com>
2022-03-13 07:15:59 +0900

    init

---
+++ src/main/java/com/eddicorp/javabackendboilerplate/JavaBackendBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.eddicorp.javabackendboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackendBoilerplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eddicorp/javabackendboilerplate/JavaBackendBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddicorp.javabackendboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendBoilerplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
